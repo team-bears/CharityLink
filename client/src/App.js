c18c442698916360bb2f660a93d61f40452e3170
RaviGhaghada <ravighaghada28@gmail.com>
2020-04-19 17:54:08 +0100

    added extremely basic login box

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 import logo from './logo.svg';
 import './App.css';
 
+import LoginBox from './components/LoginBox';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+})
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-	</p>
-        <a
-          className="App-link"
-          href="https://www.highpolarbear.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        High Polar Bear 
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client = {client}>
+      <div className="App">
+        <header className="App-header">
+          <LoginBox/>
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://www.highpolarbear.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          High Polar Bear 
+          </a>
+        </header>
+      </div>
+    </ApolloProvider>
   );
 }
 
--- client/src/components/LoginBox.js
+++ client/src/components/LoginBox.js
@@ -1,1 +1,58 @@
-// code
+import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+import {flowRight as compose} from 'lodash';
+import {loginMutation} from '../gql/mutations';
+
+class LoginBox extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            error: ''
+        };
+    }
+    submitForm(e){
+        e.preventDefault();
+
+        this.props.loginMutation({
+            variables: {
+                email: this.state.email,
+                password: this.state.password
+            },
+        }).then( (res) => {
+            alert("Hey, " + res.data.login.first_name);
+        }).catch( (err) => {
+            const error = err.graphQLErrors[0].message;
+            this.setState({error: error});
+        })
+    }
+    showError(){
+        if (this.state.error == ''){
+            return;
+        }
+        else {
+            return (<div><span style={{color: 'red'}}>Whoopsie, {this.state.error}</span></div>);
+        }
+    }
+    render() {
+        return(
+            <form id="login-box" onSubmit={this.submitForm.bind(this)}>
+                <div className="field">
+                    <label>Email:</label>
+                    <input type="text" onChange={ (e) => this.setState({ email: e.target.value }) } />
+                </div>
+                <div className="field">
+                    <label>Password:</label>
+                    <input type="text" onChange={ (e) => this.setState({ password: e.target.value }) } />
+                </div>
+                { this.showError()}
+                <button>+</button>
+            </form>
+        )
+    }
+}
+
+export default compose(
+    graphql(loginMutation, {name: "loginMutation"})
+)(LoginBox);
