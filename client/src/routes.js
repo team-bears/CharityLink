4ce2ae4a7d3faf2c1fbfe4e42ebb6cadfcf4f26f
k1895389 <terry.phung@gmail.com>
2020-04-26 17:49:22 +0100

    restructure to -> pages

--- client/src/routes.js
+++ client/src/routes.js
@@ -8,13 +8,6 @@ export default function Routes() {
   return (
     <Router>
       <div>
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <IndexApp />
@@ -26,6 +19,16 @@ export default function Routes() {
       </div>
     </Router>
   );
+}
+
+{
+  /*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */
 }
 
 /* EXAMPLE MENU LINKS
